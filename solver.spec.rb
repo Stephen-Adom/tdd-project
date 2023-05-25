967f0fa981191350baa7e54d19d3f1cffa5a5fb9
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-05-25 10:44:55 +0000

    create test case for factorial method

---
+++ solver.spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require_relative 'solver'
+
+describe Solver do
+  before(:each) do
+    @solver = Solver.new
+  end
+
+  context 'When solver class is run' do
+    context 'the factorial method' do
+      it 'should return the factorial of the argument passed' do
+        expect(@solver.factorial(5)).to eql 120
+      end
+
+      it 'should return an argument error when a negative value is passed' do
+        expect { @solver.factorial(-2) }.to raise_error(ArgumentError, 'A negative error passed as an argument')
+      end
+    end
+  end
+end
